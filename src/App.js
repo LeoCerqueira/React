93b4a4783d16398ffbfcb1db139d073953a27308
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-03-16 11:57:07 -0300

    Learning React

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import {render} from "react-dom";
+
+import SearchParams from './SearchParams'
 
 
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Rara",
-  //     animal: "Dog",
-  //     breed: "Border Collie",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Xodo",
-  //     animal: "Dog",
-  //     breed: "Labrador",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Coscuz",
-  //     animal: "Cat",
-  //     breed: "Manicum",
-  //   }),
-  // ]);
+ 
 
   return(
     <div>
       <h1 id="something-important">Adopt me!</h1>
-      <Pet name="Rara" animal="Dog" breed="Border Collie"/>
-      <Pet name="Xodó" animal="Dog" breed="Golden"/>
-      <Pet name="Coscuz" animal="Cat" breed="Unknow"/>
+      <SearchParams />
     </div>
   )
 };
-ReactDOM.render(<App/>, document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
