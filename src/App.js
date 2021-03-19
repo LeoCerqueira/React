7be707fe24ef511f11ce145d461589abef717a89
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-03-19 09:53:28 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {render} from "react-dom";
-
-import SearchParams from './SearchParams'
-
-
+import { render } from "react-dom";
+import { Router } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
- 
-
-  return(
-    <div>
-      <h1 id="something-important">Adopt me!</h1>
-      <SearchParams />
-    </div>
-  )
+  return (
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt me!</h1>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
+  );
 };
-render(<App/>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
